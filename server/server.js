4edd331d05d0171d5580b626fb1f2807c65e883d
Teodor <theodor.rotaru@gmail.com>
2023-03-15 19:11:38 +0200

    Added deployment scripts

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('express-async-errors')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const connectDB = require('./db/connectDB')
-// const path = require('path')
+const path = require('path')
 // middleware Imports
 const customError = require('./middleware/customError')
 const authMiddleware = require('./middleware/authMiddleware')
@@ -16,17 +16,18 @@ app.use(cookieParser())
 const userRouter = require('./routes/userRouter')
 const vaultRouter = require('./routes/vaultRouter')
 
-// app.use(express.static(path.resolve(__dirname, '../client/dist')))
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
 
 app.use('/api/v1/auth', userRouter)
 app.use('/api/v1/vault', authMiddleware, vaultRouter)
 
 app.use(customError)
 
+const PORT = process.env.PORT || 3000
 const start = async () => {
   try {
     await connectDB()
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
       console.log('server is listening')
     })
   } catch (error) {
